93a71e8cdfd36139c03719a6f169ef2480cacad5
ichise.masashi <ichise.masashi@gmail.com>
2018-08-19 20:04:28 +0900

    next chapter adding.

--- src/generative_art_tut/gen_2_3.clj
+++ src/generative_art_tut/gen_2_3.clj
@@ -1,36 +1,58 @@
+(ns generative_art_tut.gen_2_3
+  (:require [quil.core :as q]
+            [quil.helpers.seqs :as h]))
+
 ;;;;; Processing用のソース;;;;;
 ; フレームループを利用。setup()とdraw()
 ; -- 設定 --
-int diam = 10; // 円の直径
-float centX, centY;
-
-void setup() { // 始めに呼ばれる関数
-	size(500,300);
-	frameRate(24); // 毎秒24フレーム
-	smooth();
-	background(180);
-	centX = width / 2;
-	centY = height / 2;
-	stroke(0);
-	strokeWeight(5);
-	fill(255, 50);
-}
-void draw() { // frameRate()で指定された数だけ呼ばれる関数
-	if (diam <= 400) { // 直径のサイズが小さいこと
-		background(180); //背景をクリア
-		ellipse(centX, centY, diam, diam);
-		diam += 10; // フレームループのたびに半径を広げる。
-	}
-}
-; background(230, 230, 230);  背景色をグレーに設定
-; -- 2本の交差した直線を描く --
-; stroke(130, 0, 0); 線の色を赤に設定(RGBのRだけ値があるから。)
-; strokeWeight(4); 線の太さを4ピクセルに設定
-; line(width/2 - 70, height/2 - 70, width/2 + 70, height/2 + 70);設定したstrokeのスタイルで線を引く。
-;   引数は(始点のx座標, 始点のy座標, 終点のx座標, 終点のy座標)
-;   width, heightはそれぞれ、幅と高さ
-; line(width/2 + 70, height/2 - 70, width/2 - 70, height/2 + 70); 2つめの線を引く。
-; -- 円を描く --
-; fill(255, 150); 円の内側を塗り潰す設定
-; ellipse(width/2, height/2, 50, 50); 円をかく。
+; int diam = 10; // 円の直径
+; float centX, centY;
+; 
+; void setup() { // 始めに呼ばれる関数
+; 	size(500,300);
+; 	frameRate(24); // 毎秒24フレーム
+; 	smooth();
+; 	background(180);
+; 	centX = width / 2;
+; 	centY = height / 2;
+; 	stroke(0);
+; 	strokeWeight(5);
+; 	fill(255, 50);
+; }
+; void draw() { // frameRate()で指定された数だけ呼ばれる関数
+; 	if (diam <= 400) { // 直径のサイズが小さいこと
+; 		background(180); //背景をクリア
+; 		ellipse(centX, centY, diam, diam);
+; 		diam += 10; // フレームループのたびに半径を広げる。
+; 	}
+; }
 ;;;; ここまで ;;;;;
+
+(defn setup []
+  (q/frame-rate 24)
+  (q/smooth)
+  (q/background 180)
+  (q/stroke 0)
+  (q/stroke-weight 5)
+  (q/fill 255 25)
+;  (let [diams (h/range-incl 10 400 10)]
+  (let [diams (range 10 400 10)]
+    (q/set-state! :diam (h/seq->stream diams)
+                  :cent-x (/ (q/width) 2)
+                  :cent-y (/ (q/height) 2))))
+
+(defn draw []
+  (let [cent-x (q/state :cent-x)
+        cent-y (q/state :cent-y)
+        diam   ((q/state :diam))]
+    (when diam
+      (q/background 180)
+      (q/ellipse cent-x cent-y diam diam))))
+
+(q/defsketch gen_2_3
+  :title "Growing circle"
+  :setup setup
+  :draw draw
+  :size [500 300]
+  :keep-on-top true)
+
