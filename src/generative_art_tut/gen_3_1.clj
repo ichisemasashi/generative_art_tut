251b33a33cf54eb1597d5348bb602e79e38d5109
ichisemasashi <ichise.masashi@gmail.com>
2018-08-28 00:13:03 +0900

    add 3.1. Strait line.

---
+++ src/generative_art_tut/gen_3_1.clj
@@ -0,0 +1,30 @@
+(ns generative_art_tut.gen_3_1
+  (:require [quil.core :as q]
+            [quil.helpers.seqs :as h]))
+
+;;;;; Processing用のソース;;;;;
+; size(500, 100);
+; background(255);
+; strokeWeight(5);
+; smooth();
+; 
+; stroke(20, 50, 70);
+; line(20,50,480,50);
+;;;; ここまで ;;;;;
+
+
+
+(defn setup []
+  (q/background 255)
+  (q/stroke-weight 5)
+  (q/smooth)
+  (q/stroke 20 50 70)
+  (q/line 20 50 480 50)
+  (q/save-frame "gen.3.1.jpg"))
+
+(q/defsketch gen_3_1
+  :title "Strait Lines"
+  :setup setup
+  :size [500 100]
+  )
+
